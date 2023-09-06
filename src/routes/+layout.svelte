2dfb13d4808b1e378fd7057740478041cc9cf158
Conrad Hoang <tcmhoang@outlook.com>
2023-09-06 00:23:25 -0400

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,54 @@
+<script>
+	import '../style.css';
+	import tmdb from '$lib/images/tmdb.svg';
+</script>
+
+<nav>
+	<a href="/">Movies Marker</a>
+	<div class="links">
+		<a href="/search">Search</a>
+		<a href="/watchlist">Watch List</a>
+		<a href="/login">Log in</a>
+	</div>
+</nav>
+
+<main>
+	<slot />
+</main>
+
+<footer>
+	<p>
+		Data provided by <a href="https://www.themoviedb.org/"
+			><img alt="The Movie DB" src={tmdb} /></a
+		>
+	</p>
+</footer>
+
+<style>
+	nav {
+		display: flex;
+		width: 100%;
+		height: 3rem;
+		align-items: center;
+		justify-content: space-between;
+		max-width: var(--column);
+		padding: 0 var(--padding);
+		color: var(--accent);
+	}
+
+	a {
+		color: inherit;
+		text-decoration: none;
+	}
+
+	.links {
+		display: flex;
+		gap: 1rem;
+	}
+
+	footer {
+		display: flex;
+		justify-content: center;
+		height: 5rem;
+	}
+</style>
