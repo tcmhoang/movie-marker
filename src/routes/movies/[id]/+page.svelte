5d09460f95375af95ee7f01775d94416a69bec1d
Conrad Hoang <tcmhoang@outlook.com>
2023-09-08 23:28:35 -0400

    embed yt iframe

--- src/routes/movies/[id]/+page.svelte
+++ src/routes/movies/[id]/+page.svelte
@@ -5,3 +5,73 @@
 </script>
 
 <Hero movie={data.movie} />
+
+<div class="column grid">
+	{#if data.trailer}
+		<iframe
+			src="https://www.youtube.com/embed/{data.trailer.key}"
+			frameborder="0"
+			title="Youtube video player"
+			allow="acclerometer ; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+			allowfullscreen
+		/>
+	{/if}
+
+	<dl>
+		<dt>Released</dt>
+		<dd>
+			{data.movie.release_date}
+		</dd>
+		<dt>Runtime</dt>
+		<dd>{data.movie.runtime} minutes</dd>
+
+		<dt>Budget</dt>
+		<dd>${Math.round(data.movie.budget / 1e6)}M</dd>
+
+		<dt>Revenue</dt>
+		<dd>${Math.round(data.movie.revenue / 1e6)}M</dd>
+
+		<dt>Genre</dt>
+		<dd>{data.movie.genres.map((g) => g.name).join(', ')}</dd>
+	</dl>
+</div>
+
+<style>
+	.grid {
+		display: grid;
+		margin: 2em 0;
+		gap: 2em;
+	}
+
+	iframe {
+		aspect-ratio: 16/9;
+		width: 100%;
+	}
+
+	dl {
+		display: grid;
+		grid-template-columns: max-content 1fr;
+		gap: 1em;
+	}
+
+	dt {
+		text-transform: uppercase;
+		font-size: 0.8rem;
+		opacity: 0.8;
+		top: 0.2em;
+	}
+
+	dd {
+		margin: 0;
+	}
+
+	@media (min-width: 40rem) {
+		.grid {
+			grid-template: auto / 1fr 1fr;
+		}
+
+		dl {
+			height: 0;
+		}
+	}
+</style>
